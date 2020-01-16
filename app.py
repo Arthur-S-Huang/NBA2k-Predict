e10e86337f4893b9e8adb88905d772fb7b12dec6
shengya4 <shengya4@illinois.edu>
2020-01-15 17:31:25 -0800

    implemented predict button to display result page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route("/", methods=['POST'])
+def getValue():
+    threePointFG = request.form['threeFG']
+    return render_template('result.html', three=threePointFG)
 
 if __name__ == "__main__":
     app.run()
